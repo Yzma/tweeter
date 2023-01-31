3993619e14d786efb01e958307004c7766046acc
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-30 21:59:20 -0500

    Implement createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,46 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+const createTweetElement = function(tweet) {
+  return `
+    <article class="tweet">
+      <div class="tweet-header">
+        <div>
+          <img src="${tweet.user.avatars}"/>
+          <a>${tweet.user.name}</a>
+        </div>
+        <div>
+          ${tweet.user.handle}
+        </div>
+      </div>
+      <p class="tweet-text">${tweet.content.text}</p>
+      <div class="tweet-footer">
+        <a>${tweet.created_at}</a>
+        <div class="tweet-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </div>
+    </article>
+  `
+}
+
+$(document).ready(function() {
+  const tweet = createTweetElement(tweetData)
+
+  console.log(tweet)
+  $('.container').append(tweet)
+})
