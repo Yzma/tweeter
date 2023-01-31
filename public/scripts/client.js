38aa46300aa33898720e400e2195911b730dd62c
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-30 22:02:36 -0500

    Implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 const createTweetElement = function(tweet) {
   return `
@@ -41,9 +54,15 @@ const createTweetElement = function(tweet) {
   `
 }
 
-$(document).ready(function() {
-  const tweet = createTweetElement(tweetData)
+const renderTweets = function(tweets) {
+  for (let i of tweets) {
+    const tweet = createTweetElement(i)
 
-  console.log(tweet)
-  $('.container').append(tweet)
+    console.log(tweet)
+    $('.container').append(tweet)
+  }
+}
+
+$(document).ready(function() {
+  renderTweets(tweetData)
 })
